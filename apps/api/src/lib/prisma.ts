db70532d3bb3f72e476e24f26c03e090ef9aa748
haki-user <adityapratapsingh33@outlook.com>
2025-04-17 19:40:36 +0530

    prisma:6.6 version upgrade fix

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -4,7 +4,6 @@ const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
   errorFormat: 'pretty',
   datasources: { db: { url: process.env.DATABASE_URL } },
-  enableTracing: false,
 });
 
 export default prisma;
